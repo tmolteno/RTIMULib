eb7a6af6222fce20ff19e39683d9cd16db620407
Tim Molteno <tim@physics.otago.ac.nz>
2015-06-30 16:07:44 +1200

    Added copyright for changes

--- RTIMULib/RTMath.cpp
+++ RTIMULib/RTMath.cpp
@@ -3,6 +3,7 @@
 //  This file is part of RTIMULib
 //
 //  Copyright (c) 2014-2015, richards-tech
+//  Copyright (c) 2015, Tim Molteno
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of
 //  this software and associated documentation files (the "Software"), to deal in
